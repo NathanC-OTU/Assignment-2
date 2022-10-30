c43b5952c3e01415f9022a739b20383caa375f34
NathanC-OTU <114961208+NathanC-OTU@users.noreply.github.com>
2022-10-30 02:48:56 -0400

    Bootstrap installed

--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about me page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
